0ddf4cb9197bae373cd44df5a310a3f23bc429fe
Jordan Papaleo <papaleowebdev@gmail.com>
2019-04-30 17:16:28 -0700

    Added flow and removed proptypes

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,6 +26,7 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-flow`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
