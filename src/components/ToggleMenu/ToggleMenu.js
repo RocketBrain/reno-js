6dd65316343ad50a3c77a42a52686f84e4fc4476
Jordan Papaleo <papaleowebdev@gmail.com>
2019-04-30 17:15:58 -0700

    Craeted a toggle menu component

---
+++ src/components/ToggleMenu/ToggleMenu.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import styled from '@emotion/styled'
+
+type Props = {
+  onClick?: Function
+}
+
+const ToggleMenu = (props: Props) => {
+  const { onClick } = props
+  const [active, setActive] = useState(false)
+
+  function handleClick () {
+    if (onClick) onClick(!active)
+    setActive(!active)
+  }
+
+  return (
+    <SyledToggleMenu
+      active={active}
+      className="toggle-menu-component"
+      onClick={handleClick}
+    >
+      <span />
+      <span />
+      <span />
+    </SyledToggleMenu>
+  )
+}
+
+const SyledToggleMenu = styled.a`
+  cursor: pointer;
+  display: block;
+  height: 20px;
+  position: relative;
+  text-decoration: none;
+  width: 25px;
+
+  > span {
+    background: #000;
+    cursor: pointer;
+    height: 2px;
+    position: absolute;
+    top: 8px;
+    transition: top .25s;
+    width: 100%;
+
+    &:first-of-type {
+      top: ${({ active }) => active ? 8 : 0}px;;
+    }
+
+    &:last-child {
+      top: ${({ active }) => active ? 8 : 16}px;
+    }
+  }
+`
+
+export default ToggleMenu
