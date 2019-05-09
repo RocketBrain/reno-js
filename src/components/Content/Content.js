0fb4bf8f037b0d01cc63b6e7f133af291517d775
Jordan Papaleo <papaleowebdev@gmail.com>
2019-05-09 16:32:27 -0700

    centering main content

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -14,8 +14,8 @@ const Content = (props: Props) => (
 export default Content
 
 const StyledContent = styled.main`
+  align-self: center;
+  display: flex;
   flex: 1;
   max-width: 1080px;
-  /* margin-left: 50px; */
-  /* margin-right: 50px; */
 `
